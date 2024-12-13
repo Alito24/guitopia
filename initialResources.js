3f2532391eceb3fdcea93b97f8517d90c66895a9
Alito24 <147553250+Alito24@users.noreply.github.com>
2024-12-13 18:38:25 +0100

    Update initialResources.js

--- initialResources.js
+++ initialResources.js
@@ -334,12 +334,5 @@ const INITIAL_RESOURCES = {
         rarity: 'uncommon',
         image: 'img/well.png',
         amount: 0
-    },
-    'Void': {
-        id: 'void',
-        displayName: 'Void',
-        rarity: 'ancient',
-        image: 'img/void.png',
-        amount: 99
     }
 };
